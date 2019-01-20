c860b0047fed88cc573b11a3f2c44e58ab7c9d80
louizard <rumpertumpski@gmail.com>
2019-01-20 12:52:04 -0300

    add amazon ws and initial config

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mysql = require("mysql")
 const url = require("url")
 const querystring = require("querystring")
 const moment = require("moment")
+const AWS = require("aws-sdk")
+const configs = require("./configs.json")
 
 let posts = []
 
@@ -34,7 +36,7 @@ const c = new Crawler({
                     post_date: postDate,
                     search: uriQuery.s,
                     header_text: headerText
-                })                    
+                })
             })
         }
         done()
@@ -70,37 +72,75 @@ const postDateTextToYYYYMMDD = (dateText) => {
 
 const pageNumberStart = 1
 const pageNumberCount = 1
-const words = ["farma", "maqui"]
+const words = [] // ["farma", "maqui", "conta"]
 let pageList = []
 
-for (let i = 0; i < words.length; i++)
-    for (let j = pageNumberCount; j >= pageNumberStart; j--)
-        pageList.push(`http://www.themosvagas.com.br/page/${ j }/?s=${ words[i] }&a=1`)
+if (words.length) {
+    for (let i = 0; i < words.length; i++)
+        for (let j = pageNumberCount; j >= pageNumberStart; j--)
+            pageList.push(`http://www.themosvagas.com.br/page/${ j }/?s=${ words[i] }`)
+} else {
+    for (let i = pageNumberCount; i >= pageNumberStart; i--)
+        pageList.push(`http://www.themosvagas.com.br/page/${ i }`)
+}
 
 c.queue(pageList)
 c.on("drain", () => {
 
     console.log(`Creating connection... ${ moment().format("DD/MM/YYYY - HH:mm:ss") }`)
 
-    const conn = mysql.createConnection(require("./database.json"))
+    const conn = mysql.createConnection(configs.database)
     conn.connect()
 
+    const done = () => {
+        console.log(`New posts found: ${ postsInsertedCount }`)
+        conn.destroy()
+    }
+
+    let postsInsertedCount = 0
     for (let i = 0; i < posts.length; i++) {
         let post = { post_id: posts[i].post_id, post_url: posts[i].post_url, post_date: posts[i].post_date }
         conn.query(`SELECT * FROM themosvagas WHERE post_id = ${ post.post_id }`, (err, results, fields) => {
             if (err) throw err
             if (!results.length) {
+
                 conn.query("INSERT INTO themosvagas SET ?", post, (err, results, fields) => {
                     if (err) throw err
-                    if(i == posts.length - 1) conn.destroy()
+                    postsInsertedCount++
+
+                    // Send SMS
+
+                    if(i == posts.length - 1) done()
                 })
 
-                // Send SMS
-            } else if(i == posts.length - 1) conn.destroy()
+            } else if(i == posts.length - 1) done()
         })
     }
 
- })
+})
+
+AWS.config.region = configs.smsParams.region
+AWS.config.update({
+    accessKeyId: configs.aws.accessKeyId,
+    secretAccessKey: configs.aws.secretAccessKey,
+})
+
+const sendSMS = (message) => {
+    
+    const sns = new AWS.SNS();
+    const params = {
+        Message: message,
+        MessageStructure: "string",
+        PhoneNumber: configs.smsParams.phoneNumber,
+        Subject: "themosvagas"
+    }
+    
+    sns.publish(params, (err, data) => {
+        if (err) console.log(err, err.stack)
+        else console.log(data)
+    });
+
+}
 
 // running a task every 5 sec
 cron.schedule("*/5 * * * * *", () => { c.queue(pageList) })
